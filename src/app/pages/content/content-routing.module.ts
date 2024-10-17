6d441e546f52fb9632a7002d34320f11649e1927
Emre Alkım Aydil <emreufc@icloud.com>
2024-10-17 21:47:29 +0300

    Initialize project structure with modular directory tree

---
+++ src/app/pages/content/content-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ContentPage } from './content.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentPage
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ContentPageRoutingModule {}
