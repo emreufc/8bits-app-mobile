946d45fa57c3d37400bfd975fbac8df5e754ace4
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-11-18 15:41:11 +0300

    Added a scrollbar for chips

--- src/app/pages/content/search/search.page.ts
+++ src/app/pages/content/search/search.page.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
+  chips: string[] = ['Breakfast', 'Lunch', 'Dinner', 'Snack','Dessert', 'Drink']; // Chip'ler
+  selectedChip: string = '';
+
   constructor() { }
+
+  selectChip(chip: string) {
+    this.selectedChip = chip; // Seçilen chip'i güncelle
+  }
+
 
   ngOnInit() {
   }
