f554ca57290d40476aee35942c28ebb306c8de72
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-12-11 11:33:40 +0300

    Profile Edit sayfası tamamen yapıldı

--- src/app/pages/content/profile-edit/profile-edit.page.ts
+++ src/app/pages/content/profile-edit/profile-edit.page.ts
@@ -1,19 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+// profile-edit.page.ts
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile-edit',
   templateUrl: './profile-edit.page.html',
   styleUrls: ['./profile-edit.page.scss'],
 })
-export class ProfileEditPage implements OnInit {
-  user = {
-    name: "Jhanvi Dixit",
-    surname: "Dixit",
-    dateOfBirth: "1993-05-09",
-  };
-  constructor() { }
+export class ProfileEditPage {
+  firstName: string = 'Jhanvi';
+  lastName: string = 'Dixit';
+  gender: string = 'Male';
+  email: string = 'jhanvi@mymail.com';
+  mobileNumber: string = '9182198657';
+  birthday: string = '1993-04-08'; // Format: YYYY-MM-DD
 
-  ngOnInit() {
+  isEditingFirstName: boolean = false;
+  isEditingLastName: boolean = false;
+  isEditingGender: boolean = false;
+  isEditingEmail: boolean = false;
+  isEditingMobileNumber: boolean = false;
+  isEditingBirthday: boolean = false;
+
+  toggleEdit(field: string) {
+    switch (field) {
+      case 'firstName':
+        this.isEditingFirstName = !this.isEditingFirstName;
+        break;
+      case 'lastName':
+        this.isEditingLastName = !this.isEditingLastName;
+        break;
+      case 'gender':
+        this.isEditingGender = !this.isEditingGender;
+        break;
+      case 'email':
+        this.isEditingEmail = !this.isEditingEmail;
+        break;
+      case 'mobileNumber':
+        this.isEditingMobileNumber = !this.isEditingMobileNumber;
+        break;
+      case 'birthday':
+        this.isEditingBirthday = !this.isEditingBirthday;
+        break;
+    }
   }
-
 }
