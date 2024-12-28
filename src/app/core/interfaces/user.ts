40981c159416b368928e05646a2460a3d5cd98a0
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-12-29 01:22:59 +0300

    user-related interface için bir klasör açıldı

---
+++ src/app/core/interfaces/user.ts
@@ -0,0 +1,15 @@
+export interface UserRegister {
+    name: string;
+    surname: string;
+    email: string;
+    phoneNumber: string;
+    password: string;
+  }
+export interface UserProfile {
+    name: string;
+    surname: string;
+    email: string;
+    phoneNumber: string;
+    gender: string;
+    birthday: string;
+}
