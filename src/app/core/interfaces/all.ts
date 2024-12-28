3547d1c9b605a6263f975972eca05a680f5a5522
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-12-29 01:16:52 +0300

    User interface tanımlandı

---
+++ src/app/core/interfaces/all.ts
@@ -0,0 +1,9 @@
+export interface User {
+    name: string;
+    surname: string;
+    email: string;
+    phoneNumber: string;
+    password: string;
+    birthday: string;
+    gender: string;
+  }
