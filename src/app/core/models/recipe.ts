953c844119a92918bb8657bc1980cf578d2ba2f4
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-12-30 14:05:28 +0300

    created recipe.ts

---
+++ src/app/core/models/recipe.ts
@@ -0,0 +1,9 @@
+export interface RecipeSummary {
+  recipeId: number;
+  recipeName: string | any;
+  imageUrl: string | any;
+  recipeRate: number | any;
+  personCount: number | any;
+  preparationTime: number | any;
+  favouriteRecipes: boolean | any;
+}
