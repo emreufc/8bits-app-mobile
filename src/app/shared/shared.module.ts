3f15101866d6c1691aa7483d8cb96f32932212c0
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-12-31 14:44:08 +0300

    ShopList Service is added into the module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IngredientService } from '../core/services/ingredient.service';
+import { ShopListPageModule } from '../pages/content/shop-list/shop-list.module';
 
 
 
 @NgModule({
   declarations: [],
-  providers: [IngredientService],
+  providers: [IngredientService, ShopListPageModule],
   imports: [
     CommonModule
   ]
