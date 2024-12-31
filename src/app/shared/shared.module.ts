9fd0b85b1626bb480987a515c52710dd9603d432
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-12-31 11:37:06 +0300

    Ingredient service is binded to tha shared component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IngredientService } from '../core/services/ingredient.service';
 
 
 
 @NgModule({
   declarations: [],
+  providers: [IngredientService],
   imports: [
     CommonModule
   ]
