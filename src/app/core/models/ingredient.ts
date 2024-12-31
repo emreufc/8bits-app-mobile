2dde2d5bbd5c0a607208c69deb4ff270376b87a9
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-12-31 14:43:35 +0300

    yas yas yas benimki beni tanür

--- src/app/core/models/ingredient.ts
+++ src/app/core/models/ingredient.ts
@@ -1,9 +1,11 @@
 export interface Ingredient {
-    ingredientId: number;         // Malzeme ID'si
-    ingredientName: string;       // Malzeme adı
-    ingImgUrl: string;            // Malzeme görsel URL'si
-    allergenId: number;           // Alerjen ID'si
-    isDeleted: boolean;           // Silinme durumu
+    ingredientId: number | any;         // Malzeme ID'si
+    ingredientName: string | any;       // Malzeme adı
+    ingImgUrl: string | any;            // Malzeme görsel URL'
+    quantityTypeID: number | any;       // Miktar tipi ID'si
+    quanity: number | any;              // Miktar
+    allergenId: number | any;           // Alerjen ID'si
+    isDeleted: boolean | any;           // Silinme durumu
     allergen: any | null;         // Alerjen bilgisi (nullable)
   }
   
