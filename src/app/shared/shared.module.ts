9c404e187e76b12db9cdeea52a82c3777db60d28
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2025-01-01 21:21:09 +0300

    AuthUser is added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IngredientService } from '../core/services/ingredient.service';
 import { ShopListPageModule } from '../pages/content/shop-list/shop-list.module';
+import { AuthService } from '../core/services/auth.service';
 
 
 
 @NgModule({
   declarations: [],
-  providers: [IngredientService, ShopListPageModule],
+  providers: [IngredientService, ShopListPageModule, AuthService],
   imports: [
     CommonModule
   ]
