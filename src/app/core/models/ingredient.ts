b9afde2a19803c86d7665a397f31c8252dd44901
Fatmagül İlker <ilker20@itu.edu.tr>
2025-01-03 00:24:57 +0300

    Changed the ingredient interface for better use

--- src/app/core/models/ingredient.ts
+++ src/app/core/models/ingredient.ts
@@ -1,13 +1,12 @@
 export interface Ingredient {
-    ingredientId: number | any;         // Malzeme ID'si
-    ingredientName: string | any;       // Malzeme adı
-    ingImgUrl: string | any;            // Malzeme görsel URL'
-    quantityTypeIds: any[] | null;       // Miktar tipi ID'si
-    quantityTypes: any[] | null;     // Miktar tipi bilgisi (nullable)
-    quantityId: number | any;           // Miktar ID'si
-    quantity: number | any;              // Miktar
-    allergenId: number | any;           // Alerjen ID'si
-    isDeleted: boolean | any;           // Silinme durumu
-    allergen: any | null;         // Alerjen bilgisi (nullable)
-  }
-  
+  ingredientId: number | any;         // Malzeme ID'si
+  ingredientName: string | any;       // Malzeme adı
+  ingImgUrl: string | any;            // Malzeme görsel URL'
+  quantityId: number | any;           // Miktar ID'si
+  quantity: number | any;              // Miktar
+  allergenId: number | any;           // Alerjen ID'si
+  isDeleted: boolean | any;           // Silinme durumu
+  allergen: any | null;         // Alerjen bilgisi (nullable)
+  quantityTypeIds: any[] | null;       // Miktar tipi ID'si
+  quantityTypes: any[] | null;     // Miktar tipi bilgisi (nullable)
+  }
